b2c1e3dba02abb9f5080bec92c7f25991a7d95a3
Rishi Dhupar <rishid@gmail.com>
2024-02-21 10:39:18 -0500

    Switch to using schedule api

--- mbta.py
+++ mbta.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
-from pprint import pprint
+from zoneinfo import ZoneInfo
+from functools import reduce
 import requests
+import operator
+import os
+from pprint import pprint
 
 # url = 'https://api-v3.mbta.com/predictions?filter[stop]=place-sstat,place-north&filter[route_type]=2&filter[stop_sequence]=1&include=stop,route,schedule';
+
+def lookup(container, keys, default=None):
+    try:
+        return reduce(operator.getitem, keys, container)
+    except Exception as e:
+        return default
 
 def get_routes(included):
     filtered = {}
@@ -25,22 +35,13 @@ def get_stops(included):
             }
     return filtered
 
-def get_schedules(included):
-    filtered = {}
-    for option in included:
-        if option['type'] == "schedule":
-            filtered[option['id']] = {
-                "expected_departure": option['attributes']['departure_time']
-            }
-    return filtered
-
 def get_predictions(included):
     filtered = {}
     for option in included:
         if option['type'] == "prediction":
             filtered[option['id']] = {
                 "expected_arrival": option['attributes']['arrival_time'],
-                "expected_departure": option['attributes']['departure_time']
+                "expected_departure": option['attributes']['departure_time'],
             }
     return filtered
 
@@ -48,15 +49,12 @@ def get_predictions(included):
 # filter[stop]=WML-0214-01,WML-0214-02 -> Framingham inbound and outbound
 # filter[route_type]=2 -> only looks at predictions for commuter rails
 # include=stop,route,schedule -> to get information from the related stops, routes, schedules needed to the board
-# url = 'https://api-v3.mbta.com/predictions?filter[stop]=WML-0214-01,WML-0214-02&filter[route_type]=2&include=stop,route,schedule';
-
 url = 'https://api-v3.mbta.com/schedules?filter[stop]=WML-0214-01,WML-0214-02&filter[route_type]=2&include=stop,route,prediction'
 
 try:
     r = requests.get(url).json()
-    pprint(r)
-
-    msg = list()
+    # pprint(r)
+    trains = list()
 
     data = r['data']
     if len(data):
@@ -64,52 +62,63 @@ try:
 
         routes = get_routes(included)
         stops = get_stops(included)
-        # schedules = get_schedules(included)
         predictions = get_predictions(included)
 
-        # print('Routes')
-        # pprint(routes)
-
-        # print('Stops')
-        # pprint(stops)
-
-        # print('Schedules')
-        # pprint(schedules)
-
-        trains = []
         for schedule in data:
-            print('Schedule')
-            pprint(schedule)
+            # print('Schedule')
+            # pprint(schedule) 
+            schedule_id = schedule['id']         
+            relationship_route_id = schedule['relationships']['route']['data']['id']
             relationship_stop_id = schedule['relationships']['stop']['data']['id']
+
+            # start with train time based on schedule
+            time = lookup(schedule, ('attributes', 'departure_time'))
+            # If a train is terminating, departure_time is None and arrival_time is set
+            if not time:
+                time = lookup(schedule, ('attributes', 'arrival_time'), time)
+
+            # Now, if a prediction is available, use that instead
+            relationship_prediction_id = lookup(schedule, ('relationships', 'prediction', 'data', 'id')) 
+            if relationship_prediction_id:
+                time = lookup(predictions, (relationship_prediction_id, 'expected_departure'), time) # fall back to original if error
+                time = lookup(predictions, (relationship_prediction_id, 'expected_arrival'), time)           
+
+            time = datetime.strptime(time, '%Y-%m-%dT%H:%M:%S%z')
+
             train = {
-                'id': schedule['id'],
-                'status': schedule['attributes']['status'],
-                'departure_time': datetime.strptime(schedule['attributes']['departure_time'] or schedule[predictions['relationships']['schedule']['data']['id']]['expected_departure'], '%Y-%m-%dT%H:%M:%S%z'),
+                'id': schedule_id,
+                # 'status': schedule['attributes']['status'],
+                'time': time,
                 'origin': stops[relationship_stop_id]['station'],
-                # 'final_destination': 'foo',
-                'final_destination': routes[schedule['relationships']['route']['data']['id']]['direction_desinations'][schedule['attributes']['direction_id']],
+                'final_destination': routes[relationship_route_id]['direction_desinations'][schedule['attributes']['direction_id']],
                 'platform_code': stops[relationship_stop_id].get('platform_code', 'TBD'),
             }
             trains.append(train)
 
-        # Sort by combined predictions + schedule list by departureTime in ascending order
-        trains.sort(key=lambda train: train['departure_time'])
-        pprint(trains)
-
-        msg = [f"{t['departure_time']:%H:%M} {t['final_destination']}" for t in trains]
-    else:
-        msg = ['No trains']
+        # pprint(trains)
+    
 except Exception as e:
     print(f"An error occurred: {e}")
     msg = ['Error occurred', 'while fetching data']
 
-msg.append(f'As of {datetime.now():%H:%M:%S}')
+# Get _next_ 5 trains
+now = datetime.now(ZoneInfo("America/New_York"))
+if len(trains):
+    trains.sort(key=lambda train: train['time'])
+    
+    # Filter out the next 5 trains based on the current time
+    next_trains = [train for train in trains if train['time'] > now][:5]
+    msg = [f"{t['time']:%H:%M} {t['final_destination']}" for t in next_trains]
+else:
+    msg = ['No trains']
+
+msg.append(f'As of {now:%H:%M:%S}')
 message = '\n'.join(msg)
 print(message)
 
 def send_to_board(message):
     url = 'https://rw.vestaboard.com/'
-    headers = {'X-Vestaboard-Read-Write-Key': '4831f93e+138e+484c+9fec+894948aa20a0'}
+    headers = {'X-Vestaboard-Read-Write-Key': os.getenv('VESTABOARD_API_KEY')}
     payload = {"text": message}
     r = requests.post(url, headers=headers, json=payload)
     print(r.json())
@@ -121,4 +130,4 @@ def format(message):
     pprint(r.json())
 
 # format(message)
-
+# send_to_board(message)
